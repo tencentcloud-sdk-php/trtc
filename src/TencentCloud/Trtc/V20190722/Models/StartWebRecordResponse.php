30cb8af2fd74bdc68caae870800831e36b9f3cf8
tencentcloudapi <tencentcloudapi@tencent.com>
2026-06-25 11:15:01 +0800

    release 3.0.1613

--- src/TencentCloud/Trtc/V20190722/Models/StartWebRecordResponse.php
+++ src/TencentCloud/Trtc/V20190722/Models/StartWebRecordResponse.php
@@ -20,15 +20,15 @@ use TencentCloud\Common\AbstractModel;
 /**
  * StartWebRecord返回参数结构体
  *
- * @method string getTaskId() 获取录制任务的唯一Id
- * @method void setTaskId(string $TaskId) 设置录制任务的唯一Id
+ * @method string getTaskId() 获取<p>录制任务的唯一Id</p>
+ * @method void setTaskId(string $TaskId) 设置<p>录制任务的唯一Id</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
 class StartWebRecordResponse extends AbstractModel
 {
     /**
-     * @var string 录制任务的唯一Id
+     * @var string <p>录制任务的唯一Id</p>
      */
     public $TaskId;
 
@@ -38,7 +38,7 @@ class StartWebRecordResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $TaskId 录制任务的唯一Id
+     * @param string $TaskId <p>录制任务的唯一Id</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
